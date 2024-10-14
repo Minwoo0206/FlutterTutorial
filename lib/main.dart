d0061f1f337d6d6cca78df0ebb0aff4c07d4e8da
minwoo <koppie3248@gmail.com>
2024-10-15 07:59:37 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+  home: Home(),
+));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.mail
+          ),
+          label: const Text('mail me'),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.lightBlue, // Set the button color
+          ),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+        child: const Text('click'),
+      ),
+    );
+  }
+}
