bc72c33ec18bc9541c3f911fdb192665ad6d6825
minwoo <koppie3248@gmail.com>
2024-11-05 13:31:03 +0900

    flutter <riverpod> done!

---
+++ flutter-riverpod/lib/providers/cart_notifier.dart
@@ -0,0 +1,43 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_files/models/product.dart';
+
+part 'cart_notifier.g.dart';
+
+// Notifier Providers
+// use when you need to update the state & notify consumers
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
+  
+  @override
+  Set<Product> build() {
+    return const {};
+  }
+
+  // methods to update state
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+  }
+
+}
+
+@riverpod
+int cartTotal (ref) {
+  final cartProducts = ref.watch(cartNotifierProvider);
+
+  int total = 0;
+
+  for (Product product in cartProducts) {
+    total += product.price;
+  } 
+
+  return total;
+}
