0dc988257d8896bf09d004ffc4ac1229af928e31
minwoo <koppie3248@gmail.com>
2024-11-06 09:11:15 +0900

    <flutter> firebase auth 50%

---
+++ flutter_fb_auth/lib/screens/welcome/sign_up.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fb_auth/shared/styled_button.dart';
+import 'package:flutter_fb_auth/shared/styled_text.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // intro text
+            const Center(child: StyledBodyText('Sign up for a new account.'),),
+            const SizedBox(height: 16,),
+
+            // email address
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16,),
+
+            // password
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Password'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a password';
+                }
+                if (value.length < 8) {
+                  return 'Password must be at least 8 chars long';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16,),
+
+            // error feedback
+
+
+            // submit button
+            StyledButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  final email = _emailController.text.trim();
+                  final password = _passwordController.text.trim();
+
+                  print(email);
+                  print(password);
+                }
+              }, 
+              child: const StyledButtonText('Sign Up')
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
